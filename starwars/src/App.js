f3fde63d637a1d9fa219f4a5d66887370fe45f54
Matthew Martin <zhintateo19924559@gmail.com>
2020-04-17 22:27:52 -0500

    MVP Completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Container, Row } from 'reactstrap';
 import './App.css';
 
-const App = () => {
+import axios from 'axios';
+
+import Character from './components/Character'
+
+function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [peos, setPeo] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setPeo(response.data.results);
+        console.log(response.data)
+      })
+      .catch(err => console.log("Morty you really messed up", err))
+  }, [] )
+
+  const listItems = peos.map((peos) => <Character name={peos.name} status={peos.status} image={peos.image} />)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+
+    <>
+    
+    <Container style={{margin: '20px auto'}}>
+      <Row>
+        {listItems}
+      </Row>
+    </Container>
+    </>
   );
 }
 
